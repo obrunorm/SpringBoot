93f8cb6a8ad5d73d64b3a27991087df0f4335f21
obrunorm <87882015+obrunorm@users.noreply.github.com>
2021-09-13 13:42:26 -0300

    Add files via upload

--- blogpessoal/src/main/java/br/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ blogpessoal/src/main/java/br/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.generation.blogpessoal.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,36 +12,38 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-//Habilita a segurança
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
 
-	
-	//Sobreescreve de dados
-	@Override 
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
+		
+		auth.inMemoryAuthentication()
+		.withUser("root")
+		.password(passwordEncoder().encode("root"))
+		.authorities("ROLE_USER");
+		
 	}
 
-	//codifica a senha
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
-	//Tipo void não rertorna nada, ele só define o que precisa de autentificação e o que não precisa
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/usuarios/logar").permitAll() //Não precisa de autorização ---- "/***" libera todos os usuarios
-			.antMatchers("/usuarios/cadastrar").permitAll() //Não precisa de autorização
+			.antMatchers("/usuarios/logar").permitAll()
+			.antMatchers("/usuarios/cadastrar").permitAll()
+			.antMatchers(HttpMethod.OPTIONS).permitAll()
 			.anyRequest().authenticated()
 			.and().httpBasic()
 			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and().cors() //Cross origin
-			.and().csrf().disable(); //Desabilita porque já está gerando token na hora do login
+			.and().cors()
+			.and().csrf().disable();
 	}
 }
